10a2347d78c3591d467c33891b6d12820dac7f44
Batunde Abioro <batunde.abioro@gmail.com>
2024-06-27 21:57:54 +0100

    home work stuff

---
+++ Assignment.py
@@ -0,0 +1,38 @@
+r=1200
+g=300
+t=150
+e=100
+m=50
+total=r+g+t+e+m
+print(total)
+daily= total/30
+print(daily)
+
+
+numbers=2+4+6+8+10
+print(numbers)
+mean=numbers/5
+print(mean)
+
+#Correction toasignment
+rent = 1200
+groceries = 150
+entainment = 100
+miscellaneous = 50
+
+total_expenses = rent + grocery + enttainment + miscellaneous
+daily_expenses = total_expenses / 30
+print(daily_expenses)
+
+Question_2
+degree_celsius = 30
+degree_celsius = 100
+degree_celsius = 32.5
+degree_fahrenhiet = ( (9/5) * degree_celsius) + 32
+print(degree_fahrenhiet)
+
+Question_3
+current_year = 2024
+birth_year = 2000
+age = (current_year - birth_year)
+print(age)
